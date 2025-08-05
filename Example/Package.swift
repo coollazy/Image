251c74fd6ff865c2becb47447e4983b7a3e76751
Ven <ven.wu@baifu-tech.net>
2025-08-05 16:59:44 +0800

    [Feature] 加上 Example

---
+++ Example/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "Example",
+    platforms: [
+       .macOS(.v10_15)
+    ],
+    products: [
+        .executable(name: "App", targets: ["App"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/coollazy/Image.git", .upToNextMinor(from: "1.0.0")),
+    ],
+    targets: [
+        .target(
+            name: "App",
+            dependencies: [
+                .product(name: "Image", package: "Image"),
+            ])
+    ]
+)
